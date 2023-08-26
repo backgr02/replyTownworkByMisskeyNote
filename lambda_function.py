f2b6381f16c1a4c5a493574d7854f016038ab56a
みぞ <143225303+backgr02@users.noreply.github.com>
2023-08-26 16:48:51 +0900

    Remove token hardcoding

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from misskey import Misskey
 
@@ -13,9 +14,10 @@ def post(body: dict):
         return
 
     # Input instance address (If leaved no attribute, it sets "misskey.io")
+    #TODO: Set Misskey token in environment variable to AWS Lambda
     mk = Misskey(
         address = "misskey.io",
-        i       = "token", #FIXME: token
+        i       = os.environ['MISSKEY_TOKEN'],
     )
 
     # Let's note!
@@ -34,12 +36,3 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
-
-
-# def test():
-#     print("test")
-#     body = json.loads('{"hookId":"9iusl425pe","userId":"9d6emxmmgw","eventId":"5b0a5279-32ff-4adc-af54-5b816e78ae96","createdAt":1693006528526,"type":"mention","body":{"note":{"id":"9iusor0et7","createdAt":"2023-08-25T23:35:28.190Z","userId":"9d6emxmmgw","user":{"id":"9d6emxmmgw","name":":_mi::_zo::_ti::_n:(๑╹ω╹๑)","username":"backgr02","host":null,"avatarUrl":"https://proxy.misskeyusercontent.com/avatar.webp?url=https%3A%2F%2Fs3.arkjp.net%2Fmisskey%2Fwebpublic-1af0d1ad-68eb-4086-9c93-142e35949f7b.webp&avatar=1","avatarBlurhash":"eHMbO:?I0vBxpoUNKZ,Kx@sVCWH^^Axuik~WTV9YRjVuIC-;$SIoxa","isBot":false,"isCat":true,"emojis":{},"onlineStatus":"online","badgeRoles":[{"name":"FANBOX サポーター","iconUrl":"https://s3.arkjp.net/misskey/6e3b469e-16ed-4cc2-9098-215b441da254.png","displayOrder":0}]},"text":"**:ablobcatblink: 歯痒いにゃー**\\nhttps://misskey.io/play/9iuojjiewk\\n@backgr02 #痒い","cw":null,"visibility":"public","localOnly":false,"reactionAcceptance":"nonSensitiveOnly","renoteCount":0,"repliesCount":0,"reactions":{},"reactionEmojis":{},"tags":["痒い"],"fileIds":[],"files":[],"replyId":null,"renoteId":null,"mentions":["9d6emxmmgw"]}}}')
-#     post(body)
-
-
-# test()
